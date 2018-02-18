9cc78a217511797a57e089237407241957b7e2d2
Agnes Ching <agnesching19@gmail.com>
2018-02-18 21:56:15 +0000

    created handler function

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const http = require('http');
-const server = http.createServer();
+const message = 'I am finally starting to understand Node.js!';
+
+const handler = (request, response) => {
+  response.writeHead(200, {"content-Type": "text/html"});
+  response.write(message);
+  response.end();
+};
+
+const server = http.createServer(handler);
 
 server.listen(3000, () => {
   console.log('Server is listening on 3000. Ready to accept requests!')
