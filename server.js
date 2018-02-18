bd7539c38e360730b3919c9d33a2ee8043b09a30
Agnes Ching <agnesching19@gmail.com>
2018-02-18 22:28:15 +0000

    file system applied on server.js

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const http = require('http');
-const message = 'I am finally starting to understand Node.js!';
+let message;
+const fs = require('fs');
 
 const handler = (request, response) => {
-  response.writeHead(200, {"content-Type": "text/html"});
-  response.write(message);
-  response.end();
+  let endpoint = request.url;
+  console.log(endpoint);
+  let method = request.method;
+  console.log(method);
+
+  if (endpoint === '/') {
+    response.writeHead(200, {"Content-Type": "text/html"});
+
+    fs.readFile(__dirname + '/public/index.html', (error, file) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    })
+  }
 };
 
 const server = http.createServer(handler);
