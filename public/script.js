2663a2be9ee7f82686b402d435fcd04a04edde22
Agnes Ching <agnesching19@gmail.com>
2018-02-19 10:21:08 +0000

    basic page works

--- public/script.js
+++ public/script.js
@@ -1,28 +1,84 @@
-$(document).ready(function() {
-    $.ajax({
-        url: '/posts',
-        dataType: 'json',
-        success: function(data) {
+if (document.readyState !== 'loading') {
+  ready();
+} else {
+  document.addEventListener('DOMContentLoaded', ready);
+}
 
-            for (var blogPost in data) {
-                var postDiv         = document.createElement('div');
-                var postText        = document.createElement('p');
-                var thumbnail       = document.createElement('img');
-                var postContainer   = document.getElementsByClassName('post-container')[0];
+const ready = () => {
+  getBlogposts('/get-posts');
 
-                thumbnail.src = "./img/logo2.png";
-                thumbnail.className = "thumbnail";
-                postText.innerHTML = data[blogPost];
-                postDiv.className = "post";
+  // send posts to server
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (event) => {
 
-                postDiv.appendChild(thumbnail);
-                postDiv.appendChild(postText);
-                postContainer.appendChild(postDiv);
-                
-            }
-        },
-        error: function(error){
-            console.log(error);
-        }
+    event.preventDefault(); // prevents the form from contacting our server automatically (we want to do it ourselves)
+    let formActionUrl = form.action; // 'form.action' is the url '/create-post'
+    let formData = new FormData(form);
+
+    postBlogposts(formActionUrl, formData);
+  });
+}
+
+/****
+ * Function definitions
+ ***/
+const postBlogposts = (url, data) => {
+  fetch(url, {
+    method: 'POST',
+    body: data
+  })
+  .then((res) => {
+    res.json()
+    .then((json) => {
+      console.log(json);
+      addBlogpostsToPage(json);
+      document.querySelector('form').reset();
+    })
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+}
+
+const getBlogposts = (url) => {
+  fetch(url, {
+    method: 'GET'
+  })
+  .then((res) => {
+    res.json()
+    .then((json) => {
+      console.log(json);
+      addBlogpostsToPage(json);
     });
-});
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+}
+
+const addBlogpostsToPage = (data) => {
+  for (let blogpost in data) {
+    if (data.hasOwnProperty(blogpost)) {
+
+      let postDiv         = document.createElement('div');
+      let postText        = document.createElement('div');
+      let postContainer   = document.querySelector('.post-container');
+
+      // put <p> tags around each separate line of blogpost, otherwise
+      // they will all run together
+      postText.innerHTML = data[blogpost].split('\n').map((item) => {
+        return '<p>'+item+'</p>';
+      }).join('');
+      postText.className = 'postBody';
+      postDiv.className = 'post';
+
+      let postDetail = document.createElement('div');
+      postDetail.className = 'postDetail'
+      postDetail.innerHTML = blogpost;
+
+      postDiv.appendChild(postText);
+      postDiv.appendChild(postDetail);
+      postContainer.appendChild(postDiv);
+    }
+  }
+}
--- server.js
+++ server.js
@@ -1,61 +1,91 @@
 const http = require('http');
 let message;
+const express = require('express');
+const app = express();
 const fs = require('fs');
-const querystring = require('querystring');
+const formidable = require('express-formidable');
+// const querystring = require('querystring');
 
-const handler = (request, response) => {
-  let endpoint = request.url;
-  console.log(endpoint);
-  let method = request.method;
-  console.log(method);
-  let allTheData = '';
+app.use(express.static("public"));
+app.use(formidable());
+app.get("/gets-posts", (request, response) => {
+  response.sendFile(__dirname + '/data/posts.json')
+});
 
-  if (endpoint === '/') {
-    response.writeHead(200, {"Content-Type": "text/html"});
+app.post("/create-post", (request, response) => {
+  fs.readFile(__dirname + "/data/posts.json", (error, file) => {
+    if (error) {
+      console.log(error);
+    }
 
-    fs.readFile(__dirname + '/public/index.html', (error, file) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      response.end(file);
-    })
-  } else if (endpoint === '/node') {
-    message = 'node';
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(message);
-    response.end();
-  } else if (endpoint === '/girls') {
-    message = 'girls';
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(message);
-    response.end();
-  } else {
-    response.writeHead(200, {"Content-Type": "image/jpg"});
+    let parsedJson = JSON.parse(file);
+    let key = Date.now();
+    let value = request.fields.blogpost;
 
-    fs.readFile(__dirname + '/public/img/image.jpg', (error, file) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      response.end(file);
-    })
-  }
-  request.on('data', (chunkOfData) => {
-    allTheData += chunkOfData;
+    parsedJson[key] = value;
+    let data = JSON.stringify(parsedJson);
+    fs.readFile(__dirname + "/data/posts.json", data, (error) => {
+      console.log(error);
+    });
   });
+});
 
-  request.on('end', () => {
-    let convertedData = querystring.parse(allTheData);
-    console.log(allTheData);
-    response.end();
-  });
-};
+// const handler = (request, response) => {
+//   let endpoint = request.url;
+//   console.log(endpoint);
+//   let method = request.method;
+//   console.log(method);
+//   let allTheData = '';
+
+//   if (endpoint === '/') {
+//     response.writeHead(200, {"Content-Type": "text/html"});
+
+//     fs.readFile(__dirname + '/public/index.html', (error, file) => {
+//       if (error) {
+//         console.log(error);
+//         return;
+//       }
+//       response.end(file);
+//     })
+
+//     request.on('data', (chunkOfData) => {
+//       allTheData += chunkOfData;
+//     });
+
+//     request.on('end', () => {
+//       let convertedData = querystring.parse(allTheData);
+//       console.log(allTheData);
+//       response.end();
+//     });
+
+//   } else if (endpoint === '/node') {
+//     message = 'node';
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write(message);
+//     response.end();
+//   } else if (endpoint === '/girls') {
+//     message = 'girls';
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write(message);
+//     response.end();
+//   } else {
+//     response.writeHead(200, {"Content-Type": "image/jpg"});
+
+//     fs.readFile(__dirname + '/public/img/image.jpg', (error, file) => {
+//       if (error) {
+//         console.log(error);
+//         return;
+//       }
+//       response.end(file);
+//     })
+//   }
+
+// };
 
 
 
-const server = http.createServer(handler);
+// const server = http.createServer(handler);
 
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log('Server is listening on 3000. Ready to accept requests!')
 });
